f72d9ee9f980441d50a7700911dd9ac88d78740f
Grayson Toliver <graysontoliver@Ajorve-M2.localdomain>
2018-11-07 18:43:00 -0800

    Labs and notes

---
+++ hello_you.py
@@ -0,0 +1,7 @@
+# hello_you.py
+firstname = input("Hi, what's your first name?: ")
+lastname = input("What's your last name?: ")
+
+print("Hello", firstname, lastname)
+print("Hello " + firstname + " " + lastname)
+print(f"Hello {firstname} {lastname}")
---
+++ lab2.py
@@ -0,0 +1,26 @@
+creature = input("Name a living thing: ")
+action = input("Name an action you did yesterday: ")
+how = input("Name a sporty adverb: ")
+color = input("What is your favorite color? ")
+house = input("Name a type of shelter: ")
+animal = input("What is your favorite kind of dinosaur? ")
+flavor = input("What's the weirdest flavor you've tried? ")
+run = input("Give an action word: ")
+adverb = input("Give your favorite adverb: ")
+speak = input("Give a verb related to speaking: ")
+nice = input("Name your synonym of 'awesome': ")
+
+
+print(f"""
+Today I went to the zoo.
+I saw a {creature} jumping up and down in its tree.
+He {action} {how} through the large tunnel that led to its {color} {house}.
+I got some peanuts and passed them through the cage to a gigantic gray {animal}
+towering above my head.
+Feeding that animal made me hungry.
+I went to get a {flavor} scoop of ice
+cream. It filled my stomach.
+Afterwards I had to {run} {adverb} to catch our bus.
+When I got home I {speak} at my mom for a
+{nice} day at the zoo.
+""")
