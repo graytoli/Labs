7a0489ce29a96fa2dd3031ecb3a2bc3fb4ead7cc
Grayson Toliver <graysontoliver@Ajorve-M2.localdomain>
2018-11-07 20:27:30 -0800

    actual edit lab 2

--- pdx_lab2.py
+++ pdx_lab2.py
@@ -1,26 +1,25 @@
-creature = input("Name a living thing: ")
-action = input("Name an action you did yesterday: ")
-how = input("Name a sporty adverb: ")
+# Mad Libs, Lab 2: first attempt
+
+animal = input("What is your favorite animal? ")
+moved = input("Name a movement verb in the past tense: ")
+slowly = input("What is a synonym of slowly? ")
 color = input("What is your favorite color? ")
-house = input("Name a type of shelter: ")
-animal = input("What is your favorite kind of dinosaur? ")
-flavor = input("What's the weirdest flavor you've tried? ")
-run = input("Give an action word: ")
-adverb = input("Give your favorite adverb: ")
-speak = input("Give a verb related to speaking: ")
-nice = input("Name your synonym of 'awesome': ")
+shelter = input("Name a type of shelter: ")
+dino = input("What is your favorite type of dinosaur? ")
+flavor = input("What's the weirdest flavor of ice cream you've had? ")
+movement = input("Give a movement verb in the present tense: ")
+quickly = input("What is an antonym of quickly? ")
+thanked = input("What is a synonym of \"thanked\" ")
+awesome = input("Give a synonym of \"awesome\": ")
 
 
 print(f"""
 Today I went to the zoo.
-I saw a {creature} jumping up and down in its tree.
-He {action} {how} through the large tunnel that led to its {color} {house}.
-I got some peanuts and passed them through the cage to a gigantic gray {animal}
-towering above my head.
+I saw a {animal} jumping up and down in its tree.
+He {moved} {slowly} through the large tunnel that led to its {color} {shelter}.
+I got some peanuts and passed them through the cage to a gigantic gray {dino} towering above my head.
 Feeding that animal made me hungry.
-I went to get a {flavor} scoop of ice
-cream. It filled my stomach.
-Afterwards I had to {run} {adverb} to catch our bus.
-When I got home I {speak} at my mom for a
-{nice} day at the zoo.
+I went to get a {flavor} scoop of ice cream. It filled my stomach.
+Afterwards I had to {movement} {quickly} to catch our bus.
+When I got home I {thanked} my mom for a {awesome} day at the zoo.
 """)
