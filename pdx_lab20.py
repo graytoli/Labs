52132635885ff1e80fcf94aaae879b5265511d37
Grayson Toliver <graysontoliver@Graysons-MacBook-Air-2.local>
2018-11-16 01:43:32 -0800

    Lab 20 edit

--- pdx_lab20.py
+++ pdx_lab20.py
@@ -6,7 +6,7 @@ def validate(credit_card):
         int_list.append(int(num_list[i]))
     check_digit = int_list.pop()
     int_list.reverse()
-    for i in range(len(int_list)):
+    for i in range(0, len(int_list), 2):
         int_list[i] *= 2
     for i in range(len(int_list)):
         if int_list[i] > 9:
