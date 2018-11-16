fb8d05a13a7becb5cc3c7943f4fa5daaf4e29187
Grayson Toliver <graysontoliver@Ajorve-M2.localdomain>
2018-11-15 21:18:06 -0800

    Lab 18

---
+++ pdx_lab18.py
@@ -0,0 +1,26 @@
+# Lab 18: Peaks and Valleys, version 1
+data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+
+def peaks(data):
+    peaks_index = []
+    for i in range(1, len(data) - 1):
+        if data[i] > data[i - 1] and data[i] > data[i + 1]:
+            peaks_index.append(i)
+    return peaks_index
+
+def valleys(data):
+    valleys_index = []
+    for i in range(1, len(data) - 1):
+        if data[i] < data[i - 1] and data[i] < data[i + 1]:
+            valleys_index.append(i)
+    return valleys_index
+
+def peaks_and_valleys(data):
+    p_v_index = []
+    p_v_index.extend(peaks(data) + valleys(data))
+    p_v_index.sort()
+    return p_v_index
+
+print(peaks(data))
+print(valleys(data))
+print(peaks_and_valleys(data))
